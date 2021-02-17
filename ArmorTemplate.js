f316211bc4cf57565699e23057c460768034885d
Chris <christianrod97@gmail.com>
2021-02-17 02:06:17 -0500

    Small code style changes

---
+++ ArmorTemplate.js
@@ -0,0 +1,9 @@
+export default class ArmorTemplate {
+    constructor(atName, characterTemplate, atAbbreviation) {
+        this.atName = atName;
+        this.characterTemplate = characterTemplate;
+        this.atAbbreviation = atAbbreviation;
+    }
+
+    
+}
